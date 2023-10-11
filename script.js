7393c56f7f3dfc03fc2266a00018074027b528a1
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-10-11 18:28:32 -0500

    added script + scriptsheet

---
+++ script.js
@@ -0,0 +1,42 @@
+/*
+// VARIABLES
+Variable is a container for info. (data)
+4 ways to Declare variable
+-Let
+-Const
+-Var  <--Don't use it
+-(none) <--Don't use it
+
+CONST --> Constant: Unchangeable
+LET --> Can be changed later on
+
+DATA TYPES
+-String --> text, has quotation marks
+-Boolean --> True or False
+-Number --> no quotation marks
+-
+
+ASSIGNMENT OPERATOR
+=
+== EVALUATE IF THIS IS TRUE
+
+let x = 5
+let y = '5'
+
+(True)
+x == y  LOOSELY equivalent
+(False)
+x === y STRICTLY equivalent, DATE TYPES must match
+
+
+
+alert('Hello World!'); Prompts user with message
+const userAnswer=prompt('Do you workout?'); Asks user a question
+console.log(userAnswer); Logs input to console
+document.write(); Using JavaScript to put something on a page
+
+CONDITIONAL LOGIC
+
+if (this is true) {execute this code}
+else {}
+*/
